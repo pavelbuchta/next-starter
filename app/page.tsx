91c899eae3d5170096190fd95f3479e0ba16522e
pavelbuchta <pavelbuchta80@gmail.com>
2024-01-27 14:36:48 +0100

    initial fix page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,3 @@
-import AboutSection from "@/components/sections/about/about";
-import FAQSection from "@/components/sections/faqs/faqs";
-import HappyClientsSection from "@/components/sections/happy-clients/happy-clients";
-import HeroSection from "@/components/sections/hero";
-import LogosSection from "@/components/sections/logos/logos";
-import ServicesSection from "@/components/sections/services/services";
-
 export default function Home() {
-  return (
-    <main>
-      <HeroSection />
-      <LogosSection />
-      <ServicesSection />
-      <FAQSection />
-      <AboutSection />
-      <HappyClientsSection />
-    </main>
-  );
+  return <main></main>;
 }
